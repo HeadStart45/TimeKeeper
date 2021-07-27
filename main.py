255a51a7cec2f27d4cb544f7d89b721cbc6e43de
Liam <liam.mcdonaldlurch@gmail.com>
2021-07-27 23:00:21 +1200

    Version0.1 complete

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 #A Simple timekeeping app. Helps to keep track of time spent on tasks
 from kivy.app import App
-from kivy.lang import builder
+from kivy.clock import Clock
+
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
 from kivy.uix.label import Label
 from kivy.uix.scrollview import ScrollView
 from kivy.uix.dropdown import DropDown
+from kivy.uix.popup import Popup
 from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.textinput import TextInput
 
-from kivy.clock import Clock
+
 
 import time as t
 from datetime import timedelta
@@ -42,41 +45,123 @@ class TaskSelector(GridLayout):
         self.task = ""
         self.dropdown = DropDown()
         self.cols = 1
-        for i in range(10):
-            btn = Button(text=str(i), size_hint_y=None, height=40)
-            btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
-            self.dropdown.add_widget(btn)
+        self.tasks = []
+        with open("tasks.txt", "r") as t:
+            raw_tasks = t.readlines()
+            for task in raw_tasks:
+                self.tasks.append(task.strip())
 
-        self.mainbtn = Button(text="Select", size_hint=(None, None))
+        self.mainbtn = Button(text="Task", size_hint_y=None, height=60)
         self.mainbtn.bind(on_release=self.dropdown.open)
         self.dropdown.bind(on_select=lambda instance, x: setattr(self.mainbtn, 'text', x))
+        self.dropdown.bind()
+
+        self.update_list()
 
         self.add_widget(self.mainbtn)
+    def update_list(self):
+        self.dropdown.clear_widgets()
+        for task in self.tasks:
+            btn = Button(text=task, size_hint_y=None, height=40)
+            btn.bind(on_release=lambda btn: self.dropdown.select(btn.text))
+            self.dropdown.add_widget(btn)
+        
+
+
+
+
+
 
 class CountDown(GridLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self.cols = 3
+        self.cols = 5
 
         self.timer = 0
-        self.open = "0"
+        self.open = "0:00:00"
 
         self.counter = Label(text=self.open)
 
-        self.button = Button(text="Add")
-        self.button.bind(on_press=self.addEntry)
+        self.start_button = Button(text="Start")
+        self.start_button.bind(on_press=self.startTimer)
 
+        self.stop_button = Button(text="Stop")
+        self.stop_button.bind(on_press=self.stopTimer)
+        
         self.taskselector = TaskSelector()
 
+        self.newTaskButton = Button(text="New", size_hint_x=0.3)
+        self.newTaskButton.bind(on_press=self.newTask)
+        self.delTaskButton = Button(text="Delete", size_hint_x=0.3)
+        self.delTaskButton.bind(on_press=self.deleteTask)
+        self.new_task_popup_content = GridLayout(cols=1)
+        self.new_task_input = TextInput()
+        self.new_task_popup_content.add_widget(self.new_task_input)
+        self.new_task_button = Button(text="Add")
+        self.new_task_button.bind(on_press=self.addNewTask)
+        
+        self.new_task_popup_content.add_widget(self.new_task_button)
+        self.new_task_popup = Popup(title="New Task", content=self.new_task_popup_content, size_hint=(None, None), size=(200, 200))
+        
+        
+
         self.add_widget(self.taskselector)
+        self.add_widget(self.delTaskButton)
+        self.add_widget(self.newTaskButton)
         self.add_widget(self.counter)
-        self.add_widget(self.button)
-        Clock.schedule_interval(self.incrementTimer, 1)
+        self.add_widget(self.start_button)
+
+    def newTask(self, instance):
+        self.new_task_input.text=""
+        self.new_task_popup.open()
+    
+    def deleteTask(self, instance):
+        current = self.taskselector.mainbtn.text
+        if current == "Task":
+            return
+        self.taskselector.tasks.pop(self.taskselector.tasks.index(current))
+        self.taskselector.mainbtn.text = "Task"
+        self.taskselector.update_list()
+    
+    def addNewTask(self, instance):
+        self.taskselector.tasks.append(self.new_task_input.text)
+        with open("tasks.txt", "w") as t:
+            for task in self.taskselector.tasks:
+                t.write(task + "\n")
+        self.taskselector.update_list()
+        self.new_task_popup.dismiss()
+
+    def startTimer(self, instance):
+        if self.taskselector.mainbtn.text == "Task":
+            return
+        
+        self.taskselector.disabled = True
+        self.newTaskButton.disabled = True
+        self.timerEvent = Clock.schedule_interval(self.incrementTimer, 1)
+        self.button = self.stop_button
+        self.remove_widget(self.start_button)
+        self.add_widget(self.stop_button)
 
 
-    def addEntry(self, instance):
-        myApp.main_screen.entries.add_entry(str(timedelta(seconds=self.timer)))
+    def stopTimer(self, instance):
+        self.timerEvent.cancel()
+        self.addEntry()
         self.timer = 0
+        self.counter.text = self.open
+
+        self.taskselector.disabled = False
+        self.newTaskButton.disabled = False
+        
+        self.remove_widget(self.stop_button)
+        self.add_widget(self.start_button)
+
+    def addEntry(self):
+        string = str(self.taskselector.mainbtn.text) + "  " + str(timedelta(seconds=self.timer))
+        myApp.main_screen.entries.add_entry(string)
+        with open("times.txt", "a") as t:
+            t.write(string + "\n")
+
+        
     def incrementTimer(self, instance):
         time = self.timer
         time += 1
